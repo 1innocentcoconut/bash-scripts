af9a247c422f80723d6558e2179cc9ab5843ecc7
1innocentcoconut <aryan70421@gmail.com>
2025-07-04 10:43:48 +0530

    Add files via upload

---
+++ usage_bashscript1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+
+word="beautiful"
+
+
+echo "linux is $word"
+
+echo "fedora is also $word"
+
+
+echo "Sony cameras are also $word"
+
+
+
+mynum=200
+
+if [ $mynum -eq 200 ]
+then
+  echo "this is not true"
+fi 
